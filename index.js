ad603be411245594cf24c02de14a100ae43dbdd3
Rahil <rahil.eliyev@gmail.com>
2020-12-20 19:45:09 +0400

    deposit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function credits(periods) {
+  var i;
+  var x = document.getElementsByClassName("period");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(periods).style.display = "block";
+}
